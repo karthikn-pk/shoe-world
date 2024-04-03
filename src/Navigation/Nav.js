3865ae62db8a19973e686dcedf5b0a74e3e26c74
karthikn-pk <pkarthikeyanp33@gmail.com>
2024-04-03 23:37:33 +0530

    unwanted variables removed

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,41 +1,47 @@
-import React from 'react'
+import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { RiUserAddFill } from "react-icons/ri";
 import { FaRegHeart } from "react-icons/fa";
-import { useState } from 'react';
+import { useState } from "react";
 
+function Nav({ handleSearch }) {
+  const [searchText, setSearchText] = useState("");
 
+  const handleInputChange = (e) => {
+    setSearchText(e.target.value);
+  };
 
-
-
-
-function Nav({handleSearch}) {
-
-  const [searchText,setSearchText]=useState("");
-
-  const handleInputChange=(e)=>{
-    setSearchText(e.target.value)
-  }
-  
   return (
-    
-    <nav className='flex justify-around items-center ml-1/6 '>
-      
-      
-      <div className=' px-3 py-2 m-3 mr-5 '>
-        <input type='text' className='border border-black p-2 rounded-md' placeholder='search your shoes' value={searchText} onChange={handleInputChange}/>
-        <button className='Rbtn ml-4' onClick={()=>{handleSearch(searchText)}}>Search</button>
+    <nav className="flex justify-around items-center ml-1/6 ">
+      <div className=" px-3 py-2 m-3 mr-5 ">
+        <input
+          type="text"
+          className="border border-black p-2 rounded-md"
+          placeholder="search your shoes"
+          value={searchText}
+          onChange={handleInputChange}
+        />
+        <button
+          className="Rbtn ml-4"
+          onClick={() => {
+            handleSearch(searchText);
+          }}>
+          Search
+        </button>
       </div>
-      <div className='flex ml-4'>
-        <a href='#' className='m-3 '><FaShoppingCart/></a>
-        <a href='#' className='m-3'><FaRegHeart/></a>
-        <a href='#' className='m-3'><RiUserAddFill/></a>
-        
+      <div className="flex ml-4">
+        <button className="m-3">
+          <FaShoppingCart />
+        </button>
+        <button className="m-3">
+          <FaRegHeart />
+        </button>
+        <button className="m-3">
+          <RiUserAddFill />
+        </button>
       </div>
-
-     
     </nav>
-  )
+  );
 }
 
 export default Nav;
--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,14 +1,6 @@
-import React from 'react'
+import React from "react";
 
-import Card from '../components/Card';
-
-
-
-function Products({result}) {
-  return (
-        <section className='flex flex-wrap ml-80 mt-0'>
-        {result}
-    </section>  
-  )
+function Products({ result }) {
+  return <section className="flex flex-wrap ml-80 mt-0">{result}</section>;
 }
 export default Products;
